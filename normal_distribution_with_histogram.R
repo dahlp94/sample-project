19eaf188dff2e47140560d2b1d037324defe632a
Pratik Dahal <mapratikdahal@gmail.com>
2024-07-30 19:35:58 -0500

    added comments in the code

--- normal_distribution_with_histogram.R
+++ normal_distribution_with_histogram.R
@@ -1,5 +1,8 @@
 set.seed(0)
 
+# generating the data from a normal distribution centered at zero
+# and with unit variance
 data <- rnorm(1000, mean = 0, sd = 1)
 
-hist(data)
+# histogram generation
+hist(data, main = paste("Histogram of ") )
